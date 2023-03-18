dc506f9ed83089cc8af92143dc3f72a2582cbeb8
sakurasakura <khavy.january@gmail.com>
2023-03-18 21:04:36 +0700

    succeed: extent baseAct to implement navigation

--- app/src/main/java/com/nhom1_ptqlyc/quizzapp/Activities/UC03_TaoQuiz.java
+++ app/src/main/java/com/nhom1_ptqlyc/quizzapp/Activities/UC03_TaoQuiz.java
@@ -2,16 +2,21 @@ package com.nhom1_ptqlyc.quizzapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+
+import com.nhom1_ptqlyc.quizzapp.DrawerBaseActivity;
 import com.nhom1_ptqlyc.quizzapp.R;
+import com.nhom1_ptqlyc.quizzapp.databinding.ActivityUc03TaoQuizBinding;
 
-public class UC03_TaoQuiz extends AppCompatActivity {
-
+public class UC03_TaoQuiz extends DrawerBaseActivity {
+    ActivityUc03TaoQuizBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uc03_tao_quiz);
-
-
+        binding= ActivityUc03TaoQuizBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        allocateActivityName("TẠO QUIZ");
     }
+
 }
